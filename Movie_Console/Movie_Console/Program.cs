dcefb8b8dcbfc37379455955986e1f07979d4f08
Anna <anna.h.hallberg.92@gmail.com>
2021-01-07 13:37:35 +0100

    Add code place for localhost

--- Movie_Console/Movie_Console/Program.cs
+++ Movie_Console/Movie_Console/Program.cs
@@ -1,46 +1,19 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Movie_Console.MovieAPI;
 
 namespace Movie_Console
 {
-    class Program
+    class Program : Movie
     {
         static HttpClient movie = new HttpClient();
 
-        static void ShowMovie(Movie movie)
+        public void ShowMovie(Movie movie)
         {
             Console.WriteLine($"ID: {Id}" +
             $"\n Movie title: {MovieTitle}" +
             $"\n Release year: {ReleaseYear}");
         }
-
-        static void Main()
-        {
-            RunAsync().GetAwaiter().GetResult();
-        }
-
-        static async Task RunAsync()
-        {
-            //localhost link
-            try
-            {
-                //Create new movie
-                Movie movie = new Movie
-                {
-                    Id="1",
-                    MovieTitle="The banana and me",
-                    ReleaseYear=2021
-                };
-                var url=await CreateMovieAsync
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-        }
-
-
     }
 }
