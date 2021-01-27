e7b8df7959a8c5642c133c5deba3b7c3423f21a9
Anna <anna.h.hallberg.92@gmail.com>
2021-01-27 11:49:54 +0100

    Add code in witch class for GetMovie

--- Movie_Console/Movie_Console/Menu/MenuChoice.cs
+++ Movie_Console/Movie_Console/Menu/MenuChoice.cs
@@ -16,13 +16,13 @@ namespace Movie_Console.Menu
             _movieService = movieService;
         }
 
-        public List<Movie> _movies = new List<Movie>()
-            {
-                new Movie { Id = "Movie_1", Title = "The big pineapple", Description = "The story about the big pineapple.", Producer="Anna Andersson", ReleaseDate = "2020" },
-                new Movie { Id = "Movie_2", Title = "The medium mango", Description = "The story about the medium mango.", Producer="", ReleaseDate = "2021" },
-                new Movie { Id = "Movie_3", Title = "The small apple", Description = "The story about the small apple.", Producer="", ReleaseDate = "2019" },
+        //public List<Movie> _movies = new List<Movie>()
+        //    {
+        //        new Movie { Id = "Movie_1", Title = "The big pineapple", Description = "The story about the big pineapple.", Producer="Anna Andersson", ReleaseDate = "2020" },
+        //        new Movie { Id = "Movie_2", Title = "The medium mango", Description = "The story about the medium mango.", Producer="", ReleaseDate = "2021" },
+        //        new Movie { Id = "Movie_3", Title = "The small apple", Description = "The story about the small apple.", Producer="", ReleaseDate = "2019" },
 
-            };
+        //    };
 
         public void DisplayMenu()
         {
@@ -41,36 +41,71 @@ namespace Movie_Console.Menu
             {
                 switch (choice)
                 {
+                    //GET
                     case "1":
+                        Console.Clear();
                         var movies = _movieService.GetMoviesAsync();
+                        List<Movie> movieList = new List<Movie>();
+                        foreach (Movie m in movieList)
+                        {
+                            movieList.Add(m);
+                        }
+
+                        Console.WriteLine(" i) ENTER ID TO GET MOVIE ");
+                        Console.WriteLine(" X) RETURN TO MAIN MENU ");
+                        Console.ReadLine();
+
+                        switch (Console.ReadLine())
+                        {
+                            case "i":
+                                //GET ID
+                                Console.Clear();
+                                //var movieId = _movieService.GetMovieAsync();
+                                break;
+
+                            case "X":
+                                //RETURN TO MAIN MENU
+                                Console.Clear();
+
+                                Console.WriteLine();
+                                break;
+                            default:
+                                Console.WriteLine();
+                                break;
+                        }
+
                         break;
-   
+
+                    //POST
+                    case "2":
+                        Console.Clear();
+
+                        Console.WriteLine("ENTER ID OF MOVIE : ");
+                        var id = Console.ReadLine();
+                        Console.WriteLine("ENTER TITLE OF MOVIE : ");
+                        var movieTitle = Console.ReadLine();
+                        Console.WriteLine("ENTER RELEASE YEAR : ");
+                        var releaseYear = Console.ReadLine();
+                        Console.WriteLine("ENTER PRODUCER : ");
+                        var producer = Console.ReadLine();
+                        Console.WriteLine("ENTER DESCRIPTION : ");
+                        var description = Console.ReadLine();
+
+                        //var movie = new Movie { Id = id, Title = movieTitle, ReleaseDate = releaseYear, Producer = producer, Description = description };
+                        break;
+                    //DELETE
+                    case "3":
+                        Console.WriteLine("ENTER ID OF MOVIE TO DELETE MOVIE: ");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
                 }
             }
         }
-        //GET
-        public void GetMovie()
-        {
-            /*Console.Clear();
-
-                    Console.WriteLine("- - - - - - - - - - - - - - ENTER ID TO GET MOVIE - - - - - - - - - - - - - - -");
-                    Console.WriteLine();
-                    Console.WriteLine("ID: 58611129-2dbc-4a81-a72f-77ddfc1b1b49 ");// For the movie: My Neighbour Totoro"
-                    Console.WriteLine();
-                    Console.WriteLine("ID: ebbb6b7c-945c-41ee-a792-de0e43191bd8 ");//For the movie: Porco Rosso
-                    Console.WriteLine();
-                    Console.WriteLine("ID: dc2e6bd1-8156-4886-adff-b39e6043af0c ");//For the movie: Spirited Away
-                    Console.WriteLine();
-                    Console.WriteLine("ID: cd3d059c-09f4-4ff3-8d63-bc765a5184fa ");//For the movie: Howl's Moving Castle
-                    Console.WriteLine();
-                    Console.WriteLine("ID: 12cfb892-aac0-4c5b-94af-521852e46d6a ");//For the movie: Grave of the Fireflies
-                    Console.WriteLine();
-                    Console.WriteLine("- - - - - - - - - - - - - - - - - - - -  - - - - - - - - - - - - - - - - - - - -");
-            
-            Console.Clear();*/
-        }
+    }
+}
         //POST
-        public void PostMovie()
+        /*public void PostMovie()
         {
             Console.WriteLine("ENTER ID OF MOVIE : ");
             var id = Console.ReadLine();
@@ -89,7 +124,7 @@ namespace Movie_Console.Menu
         //DELETE
         //public bool DeleteMovie(MovieModel model)
         //{
-        //}
-    }
-}
+        //}*/
+    
+
 
